41a3ca91f78024faf88843a6a418298b3042462a
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2018-03-12 00:04:10 +0900

    testPageシーンのカスタムクラスの作成

---
+++ Random/Random/testPageViewController.swift
@@ -0,0 +1,39 @@
+//
+//  testPageViewController.swift
+//  Random
+//
+//  Created by home on 2018/03/11.
+//  Copyright © 2018年 Swift-beginners. All rights reserved.
+//
+
+import UIKit
+
+class testPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func backToHome(_ sender: Any) {
+        // 現在のシーンを閉じて元のシーンへ戻る
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
